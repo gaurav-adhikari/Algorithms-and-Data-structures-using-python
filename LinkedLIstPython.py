b9dbd94e5d3c5ee00fc50dc257629add43a397e8
gaurav <gaurav.adhikari36@gmail.com>
2020-05-27 18:47:47 +0545

    LinkedList search,remove and count implementaition

--- LinkedLIstPython.py
+++ LinkedLIstPython.py
@@ -20,13 +20,96 @@ class LinkedListNode:
         self.headData = myNewHeadData
 
 
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        tempNode = LinkedListNode(item)
+        tempNode.setNextData(self.head)
+        self.head = tempNode
+
+    def getListSize(self):
+        currentNode = self.head
+        nodeCount = 0
+
+        while currentNode != None:
+            nodeCount += 1
+            currentNode = currentNode.getNextData()
+
+        return nodeCount
+
+    def isEmpty(self):
+        return self.head == None
+
+    def searchList(self, searchItem):
+
+        currentNode = self.head
+        traversalPosition = 0
+
+        while currentNode != None:
+
+            traversalPosition += 1
+
+            if currentNode.getHeadData() == searchItem:
+                return traversalPosition
+
+            currentNode = currentNode.getNextData()
+
+        return -1
+
+    def removeNode(self, removeItem):
+
+        currentNode = self.head
+        isItemFound = False
+        previousNode = None
+
+        # Considering the fact that no two datas are repeated in a list
+        while not isItemFound:
+
+            if removeItem == currentNode.getHeadData():
+                isItemFound = True
+
+            else:
+                previousNode = currentNode
+                currentNode = currentNode.getNextData()
+
+        if previousNode == None:
+            self.head = currentNode.getNextData()
+        else:
+            previousNode.setNextData(currentNode.getNextData())
+
 
 def main():
-    mylist = LinkedListNode('A')
-    mylist.setNextData('B')
-    print(mylist.getHeadData())
-    print(mylist.getNextData())
+
+    myLinkedList = UnorderedList()
+
+    myLinkedList.add('A')
+    myLinkedList.add('B')
+    myLinkedList.add('C')
+    myLinkedList.add('D')
+    myLinkedList.add('E')
+    myLinkedList.add('F')
+
+    assert myLinkedList.isEmpty() != None
+
+    assert myLinkedList.getListSize() == 6
+
+    # As new nodes gets added on the precceeding node so A is the last element
+    assert myLinkedList.searchList('A') == 6
+    assert myLinkedList.searchList('G') == -1
+
+    myLinkedList.removeNode('C')
+    myLinkedList.removeNode('B')
+
+    assert myLinkedList.searchList('B') == -1
+    assert myLinkedList.searchList('C') == -1
+
+    assert myLinkedList.getListSize() == 4
     pass
+
+
 
 if __name__ == '__main__':
     main()
