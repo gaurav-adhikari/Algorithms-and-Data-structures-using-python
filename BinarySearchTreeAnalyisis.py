0652ed0619e571e99955f6f938ceed7653fb4254
gaurav <gaurav.adhikari36@gmail.com>
2020-07-06 07:57:20 +0545

    initial BST data collect

---
+++ BinarySearchTreeAnalyisis.py
@@ -0,0 +1,41 @@
+# Binary Tree Analysis in python
+
+'''
+
+        a
+
+b<a          c>a
+
+-> Build a helper class to keep track of nodes
+
+'''
+class BST:
+    
+    def __init__(self):
+        pass
+
+
+
+'''
+
+    Node
+
+left        right
+
+
+-> every child carries a value or a payload
+-> information about left child and right child
+
+'''
+class Node:
+    def __init__(self):
+        pass
+
+
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
